abc52ff48f396daa1b270240a0fd9e13f27fd460
Lavith Raj <63673413+lraj22@users.noreply.github.com>
2023-12-21 20:07:22 -0800

    Add index.js and use socket.io

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
+const socketio = require('socket.io');
 
-http.createServer(function (req, res) {
-    fs.readFile(__dirname + '/index.html', function (err, data) {
+const server = http.createServer(function (req, res) {
+    var path = url.parse(req.url).pathname;
+    if (path == "/") {
+        path = "/index.html";
+    }
+    fs.readFile(__dirname + path, function (err, data) {
         if (err) {
-            res.writeHead(500);
+            res.writeHead(404);
             res.end();
         } else {
             res.writeHead(200, { 'Content-Type': 'text/html' });
@@ -12,3 +18,12 @@ http.createServer(function (req, res) {
         }
     });
 }).listen(process.env.PORT || 8080);
+
+const io = new socketio.Server(server);
+
+io.on("connection", (socket) => {
+    socket.on("serverTest", function (info, callback) {
+        console.log(info);
+        callback(true);
+    });
+});
